7cbb332ea8b696e208b7c54825cc03f1c7379770
monta <djmonta@gmail.com>
2018-10-05 20:37:10 +0900

    :wrench: config again

--- config/website.js
+++ config/website.js
@@ -2,11 +2,11 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'Shimarl Design', // Navigation and Site Title
   siteTitleAlt: 'Shimarl Design - Creative Partner', // Alternative Site title for SEO
-  siteUrl: 'https://modest-kepler-3f5f54.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://shimarl-design.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'ja', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: '',
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  author: 'S', // Author for schemaORGJSONLD
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@shimardesign', // Twitter Username
   ogSiteName: 'Shimarl Design', // Facebook Site Name
